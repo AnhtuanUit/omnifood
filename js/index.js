5d9610cf5a1ff0d1afa999ff4ba12cb5f49fde40
TuanDo <anhtuan7194@gmail.com>
2024-01-03 14:55:49 +0700

    Implement a Sticky Navigation

--- js/index.js
+++ js/index.js
@@ -8,3 +8,25 @@ if (btnMenyEl) {
     header.classList.toggle("nav-open");
   });
 }
+
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 1,
+    // rootMargin: "-80px",
+  }
+);
+
+obs.observe(sectionHeroEl);
